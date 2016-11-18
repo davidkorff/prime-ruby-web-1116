c60ef071f699d36a002959b7605f26f72bed2e32
David Korff <davidkorff@gmail.com>
2016-11-18 18:22:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+# Add  code here
+def prime?(integer)
+  n=2
+  if integer<0
+    integer = integer * -1
+  else
+    integer = integer 
+  end
+
+  while n< integer
+    if integer%n == 0
+      return false
+    else
+      n +=1
+    end
+  end
+    return true
+
+end
